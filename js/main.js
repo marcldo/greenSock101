6a31bccec4cf36ecab289abb6dbe73e2dcf61c9a
gitMarcldo <marcldo@users.noreply.github.com>
2020-10-06 19:35:36 -0400

    timelines

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+
+//lesson 1 getting started
 // // animate header down by 20 px for 1 second using the "to" tween
 // // to sween specify the ending point, starts at the css position
 // gsap.to('header', { duration: 1, y: 20 })
@@ -32,42 +35,82 @@
 // //"set" tween doesn't animate just moves element to the position indicated. 
 // gsap.set('ul', { y: 100 });
 
+//lesson two tween
 //first demo
-gsap.from(
-  'body',
-  {
-    backgroundColor: 'white',
-    duration: 1.7,
-    ease: 'none'
-  })
+// gsap.from(
+//   'body',
+//   {
+//     backgroundColor: 'white',
+//     duration: 1.7,
+//     ease: 'none'
+//   })
 
-gsap.from(
-  ['h1', 'p'],
-  {
-    delay: 1.5,
-    duration: 0.6,
-    y: -20,
-    opacity: 0,
-    stagger: 0.2,
-    ease: 'power1.out'
-  })
+// gsap.from(
+//   ['h1', 'p'],
+//   {
+//     delay: 1.5,
+//     duration: 0.6,
+//     y: -20,
+//     opacity: 0,
+//     stagger: 0.2,
+//     ease: 'power1.out'
+//   })
 
-gsap.from(
-  ['img', 'h2'],
-  {
-    delay: 2.8,
-    opacity: 0,
-    duration: 0.7,
-    ease: 'none'
-  })
+// gsap.from(
+//   ['img', 'h2'],
+//   {
+//     delay: 2.8,
+//     opacity: 0,
+//     duration: 0.7,
+//     ease: 'none'
+//   })
 
-gsap.from(
-  'ul li',
-  {
-    delay: 4,
-    opacity: 0,
-    y: -20,
-    stagger: 0.2,
-    duration: 0.6,
-    ease: 'power1.out'
-  })
+// gsap.from(
+//   'ul li',
+//   {
+//     delay: 4,
+//     opacity: 0,
+//     y: -20,
+//     stagger: 0.2,
+//     duration: 0.6,
+//     ease: 'power1.out'
+//   });
+
+//lesson 3 timelines
+// declaring a timeline
+const timeline = gsap.timeline({ duration: 1 });
+
+//attaching tweens to timeline
+timeline
+  .from(
+    'body',
+    {
+      backgroundColor: 'white',
+      ease: 'none'
+    }
+  )
+  .from(
+    ['h1', 'p'],
+    {
+      y: -20,
+      opacity: 0,
+      stagger: 0.2,
+      ease: 'power1.out'
+    }
+  )
+  .from(
+    ['img', 'h2'],
+    {
+      opacity: 0,
+      ease: 'none'
+    }
+  )
+  .from(
+    'ul li',
+    {
+      opacity: 0,
+      y: -20,
+      stagger: 0.2,
+      ease: 'power1.out'
+    }
+  );
